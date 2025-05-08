a155990ba6d572a69e034f4c43aceb1a02106b56
alizhumataeyev <japangod202203@gmail.com>
2025-05-08 17:42:28 +0500

    base backend

--- be-chat-api/src/gpt-module/gpt/gpt.controller.ts
+++ be-chat-api/src/gpt-module/gpt/gpt.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { GptService } from './gpt.service';
 
 @Controller('gpt')
-export class GptController {}
+export class GptController {
+  constructor(private readonly gptService: GptService) {}
+
+  @Post()
+  async handleGptRequest(@Body('text') text: string) {
+    const response = await this.gptService.askGpt(text);
+    return { response };
+  }
+}
--- be-chat-api/src/gpt-module/gpt/gpt.service.ts
+++ be-chat-api/src/gpt-module/gpt/gpt.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
-export class GptService {}
+export class GptService {
+  private readonly apiKey = process.env.OPEN_AI_KEY!;
+  private readonly gptApiInstance : string = process.env.OPEN_AI_LINK!
+
+  async askGpt(prompt: string): Promise<string> {
+    const response = await axios.post(
+      this.gptApiInstance,
+      {
+        model: 'gpt-4o-mini',
+        messages: [{ role: 'user', content: prompt }],
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${this.apiKey}`,
+          'Content-Type': 'application/json',
+        },
+      },
+    );
+
+    return response.data.choices[0].message.content.trim();
+  }
+}
