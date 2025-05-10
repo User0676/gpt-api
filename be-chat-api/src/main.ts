8cd0a77d33694f690f4a66537f094db7c7dd7262
alizhumataeyev <japangod202203@gmail.com>
2025-05-10 17:47:35 +0500

    open cors

--- be-chat-api/src/main.ts
+++ be-chat-api/src/main.ts
@@ -3,7 +3,16 @@ import { AppModule } from './app.module';
 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: { origin: 'http://localhost:3000' } });
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: [
+        'http://localhost:3000',
+        'https://gpt-api-mocha.vercel.app',
+      ],
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true
+    }
+  });
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
