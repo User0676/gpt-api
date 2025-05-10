08267909a2ebda026d85054436d136da539d0646
alizhumataeyev <japangod202203@gmail.com>
2025-05-10 17:39:28 +0500

    stage

--- be-chat-api/src/main.ts
+++ be-chat-api/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: { origin: 'http://localhost:3000' } });
   await app.listen(process.env.PORT ?? 3001);
